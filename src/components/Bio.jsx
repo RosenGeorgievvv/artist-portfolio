97a14f295fdc650b0140ae30b83c35f8b8527091
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-09-13 18:56:44 +0300

    Update Bio.jsx

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -3,7 +3,7 @@ import {motion} from 'framer-motion';
 
 const Bio = () => {
   return (
-    <section className="flex max-w-4xl flex-col gap-12 pt-20" id="bio">
+    <section className="flex max-w-4xl flex-col gap-12 pt-1" id="bio">
         <motion.h2 initial={{ opacity: 0, y: -20}} whileInView={{opacity: 1, y: 0}} transition={{duration: 1}} className="text-center text-3xl lg:text-4xl">Bio</motion.h2>
         <motion.div initial={{opacity: 0}} whileInView={{ opacity: 1}} transition={{duration: 0.8, delay: 0.2}}>
             {BIO.map((bio, index) =>(
