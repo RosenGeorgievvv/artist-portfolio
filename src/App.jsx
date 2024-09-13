159ae6781a2231de4fe3dc33c88702d23bc115d3
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-09-13 18:03:47 +0300

    project update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import './App.css'
+import React from "react";
+import Profile from "./components/Profile";
+import Navbar from "./components/Navbar";
+import Songs from './components/Songs';
+import Bio from './components/Bio';
+import Footer from "./components/Footer";
 
-function App() {
-
+const App = () => {
   return (
-    <>
-      Artist portfolio
-    </>
-  )
-}
+    <div className="relative h-full overflow-y-auto antialiased">
+      <div className="fixed inset-0 bg-fixed bg-cover bg-center bg-img"></div>
+        <div className="relative z-10 flex flex-col items-center p-4 space-y-8 container mx-auto">
+          <Profile />
+          <Navbar />
+          <Songs />
+          <Bio />
+          <Footer />
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
